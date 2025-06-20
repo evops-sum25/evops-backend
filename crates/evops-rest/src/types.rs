3aecef4fd17d7e8a1b846cfcbc10d26202d299f0
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-20 03:18:01 +0300

    document schemas

--- crates/evops-rest/src/types.rs
+++ crates/evops-rest/src/types.rs
@@ -8,11 +8,13 @@ mod conversions;
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct EventServiceFindRequest {
+    /// UUID of the event to retrieve.
     id: crate::types::EventId,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct EventServiceFindResponse {
+    /// Retrieved event object.
     event: crate::types::Event,
 }
 
@@ -21,26 +23,31 @@ pub struct EventServiceListRequest;
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct EventServiceListResponse {
+    /// List of all events.
     events: Vec<crate::types::Event>,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct EventServiceCreateRequest {
+    /// Data for creating a new event.
     form: crate::types::NewEventForm,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct EventServiceCreateResponse {
+    /// Created event object.
     event: crate::types::Event,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct TagServiceFindRequest {
+    /// UUID of the tag to retrieve.
     id: crate::types::TagId,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct TagServiceFindResponse {
+    /// Retrieved tag object.
     tag: crate::types::Tag,
 }
 
@@ -49,26 +56,31 @@ pub struct TagServiceListRequest;
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct TagServiceListResponse {
+    /// List of all tags.
     tags: Vec<crate::types::Tag>,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct TagServiceCreateRequest {
+    /// Data for creating a new tag.
     form: crate::types::NewTagForm,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct TagServiceCreateResponse {
+    /// Created tag object.
     tag: crate::types::Tag,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct UserServiceFindRequest {
+    /// UUID of the user to retrieve.
     id: crate::types::UserId,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct UserServiceFindResponse {
+    /// Retrieved user object.
     user: crate::types::User,
 }
 
@@ -77,65 +89,93 @@ pub struct UserServiceListRequest;
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct UserServiceListResponse {
+    /// List of all users.
     users: Vec<crate::types::User>,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 pub struct UserServiceCreateRequest {
+    /// Data for creating a new user.
     form: crate::types::NewUserForm,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 pub struct UserServiceCreateResponse {
+    /// Created user object.
     user: crate::types::User,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 struct NewEventForm {
+    /// UUID of the creating user.
     author_id: crate::types::UserId,
+    /// URLs of event images.
     image_urls: Option<Vec<Url>>,
+    /// Event title.
     title: crate::types::EventTitle,
+    /// Detailed description.
     description: crate::types::EventDescription,
+    /// UUIDs of associated tags.
     tag_ids: Option<Vec<crate::types::TagId>>,
+    /// Whether to enable attendance tracking.
     with_attendance: bool,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 struct Event {
+    /// Event UUID.
     id: crate::types::EventId,
+    /// User who created the event.
     author: crate::types::User,
+    /// URLs of event images.
     image_urls: Vec<Url>,
+    /// Event title.
     title: crate::types::EventTitle,
+    /// Detailed description.
     description: crate::types::EventDescription,
+    /// Associated tags.
     tags: Vec<crate::types::Tag>,
+    /// Whether attendance tracking is enabled.
     with_attendance: bool,
+    /// Creation timestamp.
     created_at: DateTime<Utc>,
+    /// Last modification timestamp.
     modified_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 struct NewTagForm {
+    /// Unique name.
     name: crate::types::TagName,
+    /// Alternative names.
     aliases: Option<Vec<crate::types::TagAlias>>,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 struct Tag {
+    /// Tag UUID.
     id: TagId,
+    /// Unique name (e.g., `"Music"`, `"Tech"`).
     name: crate::types::TagName,
+    /// Alternative names (e.g., `["Concert", "Gig"]` for `"Music"`).
     aliases: Vec<crate::types::TagAlias>,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 struct NewUserForm {
+    /// Display name.
     name: crate::types::UserName,
+    /// URL of profile image.
     profile_picture_url: Option<Url>,
 }
 
 #[derive(Debug, Serialize, JsonSchema)]
 struct User {
+    /// User UUID.
     id: crate::types::UserId,
+    /// Display name.
     name: crate::types::UserName,
+    /// URL of profile image.
     profile_picture_url: Option<Url>,
 }
 
