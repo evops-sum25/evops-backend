5150de522f4d94a34ec4bb29523529c83b18b404
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-19 02:36:08 +0300

    .

--- crates/evops-grpc/src/services/event.rs
+++ crates/evops-grpc/src/services/event.rs
@@ -149,35 +149,37 @@ impl EventService for self::Service {
 
     async fn reorder_images(
         &self,
-        request: Request<EventServiceReorderImagesRequest>,
+        _request: Request<EventServiceReorderImagesRequest>,
     ) -> Result<Response<EventServiceReorderImagesResponse>, Status> {
-        let request_data = request.into_inner();
+        // let request_data = request.into_inner();
 
-        let event_id = evops_models::EventId::new({
-            request_data
-                .event_id
-                .parse::<Uuid>()
-                .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
-        });
-        let image_order = {
-            evops_models::EventImageIds::try_new({
-                request_data
-                    .image_ids
-                    .into_iter()
-                    .map(|image_id| {
-                        ApiResult::Ok(evops_models::EventImageId::new({
-                            image_id
-                                .parse::<Uuid>()
-                                .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
-                        }))
-                    })
-                    .collect::<Result<_, _>>()?
-            })
-            .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
-        };
-        self.state.reorder_images(event_id, image_order).await?;
+        // let event_id = evops_models::EventId::new({
+        //     request_data
+        //         .event_id
+        //         .parse::<Uuid>()
+        //         .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
+        // });
+        // let image_order = {
+        //     evops_models::EventImageIds::try_new({
+        //         request_data
+        //             .image_ids
+        //             .into_iter()
+        //             .map(|image_id| {
+        //                 ApiResult::Ok(evops_models::EventImageId::new({
+        //                     image_id
+        //                         .parse::<Uuid>()
+        //                         .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
+        //                 }))
+        //             })
+        //             .collect::<Result<_, _>>()?
+        //     })
+        //     .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
+        // };
+        // self.state.reorder_images(event_id, image_order).await?;
 
-        Ok(Response::new(EventServiceReorderImagesResponse {}))
+        // Ok(Response::new(EventServiceReorderImagesResponse {}))
+
+        Err(Status::unimplemented(""))
     }
 
     async fn push_image(
@@ -288,18 +290,20 @@ impl EventService for self::Service {
 
     async fn delete_image(
         &self,
-        request: Request<EventServiceDeleteImageRequest>,
+        _request: Request<EventServiceDeleteImageRequest>,
     ) -> Result<Response<EventServiceDeleteImageResponse>, Status> {
-        let request_data = request.into_inner();
+        // let request_data = request.into_inner();
 
-        let image_id = evops_models::EventImageId::new({
-            request_data
-                .image_id
-                .parse::<Uuid>()
-                .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
-        });
-        self.state.delete_image(image_id).await?;
+        // let image_id = evops_models::EventImageId::new({
+        //     request_data
+        //         .image_id
+        //         .parse::<Uuid>()
+        //         .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
+        // });
+        // self.state.delete_image(image_id).await?;
 
-        Ok(Response::new(EventServiceDeleteImageResponse {}))
+        // Ok(Response::new(EventServiceDeleteImageResponse {}))
+
+        Err(Status::unimplemented(""))
     }
 }
--- crates/evops-grpc/src/services/language.rs
+++ crates/evops-grpc/src/services/language.rs
@@ -1,7 +1,5 @@
 use tonic::{Request, Response, Status};
 
-use evops_models::ApiError;
-
 use crate::AppState;
 use crate::pb::language_service_server::{LanguageService, LanguageServiceServer};
 use crate::pb::{LanguageServiceAddRequest, LanguageServiceAddResponse};
@@ -11,6 +9,7 @@ pub fn server(state: AppState) -> LanguageServiceServer<self::Service> {
 }
 
 pub struct Service {
+    #[allow(dead_code)]
     state: AppState,
 }
 
@@ -18,26 +17,28 @@ pub struct Service {
 impl LanguageService for self::Service {
     async fn add(
         &self,
-        request: Request<LanguageServiceAddRequest>,
+        _request: Request<LanguageServiceAddRequest>,
     ) -> Result<Response<LanguageServiceAddResponse>, Status> {
-        let request_data = request.into_inner();
+        // let request_data = request.into_inner();
 
-        let form = {
-            request_data
-                .form
-                .ok_or_else(|| {
-                    ApiError::InvalidArgument({
-                        let err_msg = "LanguageServiceAddRequest.form must not be null.";
-                        err_msg.to_owned()
-                    })
-                })?
-                .try_into()?
-        };
-        let new_language_id = self.state.add_language(form).await?;
+        // let form = {
+        //     request_data
+        //         .form
+        //         .ok_or_else(|| {
+        //             ApiError::InvalidArgument({
+        //                 let err_msg = "LanguageServiceAddRequest.form must not be null.";
+        //                 err_msg.to_owned()
+        //             })
+        //         })?
+        //         .try_into()?
+        // };
+        // let new_language_id = self.state.add_language(form).await?;
 
-        let response_data = LanguageServiceAddResponse {
-            language_id: new_language_id.into_inner().to_string(),
-        };
-        Ok(Response::new(response_data))
+        // let response_data = LanguageServiceAddResponse {
+        //     language_id: new_language_id.into_inner().to_string(),
+        // };
+        // Ok(Response::new(response_data))
+
+        Err(Status::unimplemented(""))
     }
 }
