332eb2affab6d0253756ec9280a4592af6a725fc
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-19 02:33:13 +0300

    .

--- crates/evops-grpc/src/services/user.rs
+++ crates/evops-grpc/src/services/user.rs
@@ -1,10 +1,12 @@
 use tonic::{Request, Response, Status};
-use uuid::Uuid;
 
 use evops_models::ApiError;
 
 use crate::pb::user_service_server::{UserService, UserServiceServer};
-use crate::pb::{UserServiceCreateResponse, UserServiceFindResponse, UserServiceListResponse};
+use crate::pb::{
+    UserServiceLogInRequest, UserServiceLogInResponse, UserServiceRefreshRequest,
+    UserServiceRefreshResponse, UserServiceSignUpRequest, UserServiceSignUpResponse,
+};
 
 pub fn server(state: crate::AppState) -> UserServiceServer<self::Service> {
     UserServiceServer::new(self::Service { state })
@@ -16,60 +18,27 @@ pub struct Service {
 
 #[tonic::async_trait]
 impl UserService for self::Service {
-    async fn find(
+    async fn log_in(
         &self,
-        request: Request<crate::pb::UserServiceFindRequest>,
-    ) -> Result<Response<crate::pb::UserServiceFindResponse>, Status> {
+        request: Request<UserServiceLogInRequest>,
+    ) -> Result<Response<UserServiceLogInResponse>, Status> {
         let request_data = request.into_inner();
-
-        let id = {
-            evops_models::UserId::new({
-                request_data
-                    .id
-                    .parse::<Uuid>()
-                    .map_err(|e| ApiError::InvalidArgument(e.to_string()))?
-            })
-        };
-        let found_user = self.state.find_user(id).await?;
-
-        let response_data = UserServiceFindResponse {
-            user: Some(found_user.into()),
-        };
-        Ok(Response::new(response_data))
+        todo!();
     }
 
-    async fn list(
+    async fn refresh(
         &self,
-        _request: Request<crate::pb::UserServiceListRequest>,
-    ) -> Result<Response<crate::pb::UserServiceListResponse>, Status> {
-        let users = self.state.list_users().await?;
-
-        let response_data = UserServiceListResponse {
-            users: users.into_iter().map(Into::into).collect(),
-        };
-        Ok(Response::new(response_data))
+        request: Request<UserServiceRefreshRequest>,
+    ) -> Result<Response<UserServiceRefreshResponse>, Status> {
+        let request_data = request.into_inner();
+        todo!();
     }
 
-    async fn create(
+    async fn sign_up(
         &self,
-        request: Request<crate::pb::UserServiceCreateRequest>,
-    ) -> Result<Response<crate::pb::UserServiceCreateResponse>, Status> {
+        request: Request<UserServiceSignUpRequest>,
+    ) -> Result<Response<UserServiceSignUpResponse>, Status> {
         let request_data = request.into_inner();
-
-        let form = {
-            request_data
-                .form
-                .ok_or({
-                    let err_msg = "UserServiceCreateRequest.form must not be null.";
-                    ApiError::InvalidArgument(err_msg.to_owned())
-                })?
-                .try_into()?
-        };
-        let user_id = self.state.create_user(form).await?;
-
-        let response_data = UserServiceCreateResponse {
-            user_id: user_id.to_string(),
-        };
-        Ok(Response::new(response_data))
+        todo!();
     }
 }
